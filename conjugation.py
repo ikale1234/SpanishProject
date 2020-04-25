cb903689408c5585efd850f87543856748399878
ikale1234 <55333848+ikale1234@users.noreply.github.com>
2020-04-24 18:44:31 -0700

    changed game into class

--- conjugation.py
+++ conjugation.py
@@ -5,142 +5,138 @@ import sys
 points = 0
 
 
-def betweenStatements(statement):
-    print("\n")
-    print(statement)
-    input("\nPress Enter to Continue:")
+class Game:
+    def __init__(self):
+        self.points = 0
+        self.listdata = []
+        self.points = 0
 
+    def betweenStatements(self, statement):
+        print("\n")
+        print(statement)
+        input("\nPress Enter to Continue:")
 
-def getList():
-    if len(sys.argv) == 1:
-        alldata = verborganizer.build("SpanishWords")
-        listdata = alldata["Verbs"]
-    elif len(sys.argv) == 2:
-        try:
-            listdata = verborganizer.build(sys.argv[1])
-        except FileNotFoundError:
+    def getList(self):
+        if len(sys.argv) == 1:
+            self.alldata = verborganizer.build("SpanishWords")
+            self.listdata = self.alldata["Verbs"]
+        elif len(sys.argv) == 2:
+            try:
+                self.listdata = verborganizer.build(sys.argv[1])
+            except FileNotFoundError:
+                print("That is an invalid directory.")
+                quit()
+        else:
             print("That is an invalid directory.")
             quit()
-    else:
-        print("That is an invalid directory.")
-        quit()
-    return listdata
+
+    def getGameValues(self):
+        self.samplelist = []
+        for idx in range(3):
+            if idx == 0:
+                self.i = random.randrange((len(self.listdata)))
+                self.k = random.randrange(0, 5)
+                self.j = "Present"
+                self.doc, self.rform = self.i, self.k
+            if idx == 1:
+                self.j = "Preterite"
+            if idx == 2:
+                self.j = "Imperfect"
+            self.samplelist.append(self.listdata[self.i][self.j][self.k])
+        if self.samplelist[0] == self.samplelist[1]:
+            self.samplelist = self.samplelist.pop(0)
+
+        for idx in range(8-len(self.samplelist)):
+            self.k = random.randrange(0, 5)
+            self.j = random.choice(["Present", "Preterite", "Imperfect"])
+            while self.listdata[self.i][self.j][self.k] in self.samplelist:
+                self.k = random.randrange(0, 5)
+                self.j = random.choice(["Present", "Preterite", "Imperfect"])
+            self.samplelist.append(self.listdata[self.i][self.j][self.k])
+
+        # checks repeating
+
+        self.right = self.samplelist[random.randrange(3)]
+        if self.right == self.samplelist[0]:
+            self.rtense = "Present"
+        if self.right == self.samplelist[1]:
+            self.rtense = "Preterite"
+        if self.right == self.samplelist[2]:
+            self.rtense = "Imperfect"
+
+        random.shuffle(self.samplelist)
+
+    def getQuestionVals(self):
+        if self.rtense == "Present":
+            self.tense = "present"
+        if self.rtense == "Preterite":
+            self.tense = "preterite"
+        if self.rtense == "Imperfect":
+            self.tense = "imperfect"
+        if self.rform == 0:
+            self.form = "yo"
+        if self.rform == 1:
+            self.form = "tu"
+        if self.rform == 2:
+            self.form = random.choice(["el", "ella", "usted"])
+        if self.rform == 3:
+            self.form = "nosotros"
+        if self.rform == 4:
+            self.form = random.choice(["ellos", "ellas", "ustedes"])
+
+    def getInput(self):
+        self.guess = input("What is the "+self.tense+" tense of "+self.listdata[self.i]["Infinitive"]+" in the "+self.form+"? \nA: "+self.samplelist[0]+"\nB: "+self.samplelist[1] + "\nC: " +
+                           self.samplelist[2] + "\nD: " + self.samplelist[3] + "\nE: " + self.samplelist[4]+"\nF: " + self.samplelist[5]+"\nG: " + self.samplelist[6]+"\nH: " + self.samplelist[7]+"\n")
+        if self.right == self.samplelist[0]:
+            self.let = "A"
+        if self.right == self.samplelist[1]:
+            self.let = "B"
+        if self.right == self.samplelist[2]:
+            self.let = "C"
+        if self.right == self.samplelist[3]:
+            self.let = "D"
+        if self.right == self.samplelist[4]:
+            self.let = "E"
+        if self.right == self.samplelist[5]:
+            self.let = "F"
+        if self.right == self.samplelist[6]:
+            self.let = "G"
+        if self.right == self.samplelist[7]:
+            self.let = "H"
+
+    def checkIfRight(self):
+        self.rightlist = ["Good Job! You got it right!", "Nice! That's right!", "That is the correct answer!",
+                          "You are good at this!", "Wow! That was cool how you got it right!"]
+        self.wronglist = ["You suck! That was the wrong answer!", "How did you miss that! That was easy!",
+                          "That is the wrong answer.", "That is completely incorrect!", "Are you stupid? That was wrong!"]
+        if self.guess == self.let or self.guess == self.let.lower():
+            self.points += 1
+            self.betweenStatements(random.choice(self.rightlist))
+        else:
+            self.betweenStatements(random.choice(self.wronglist))
+
+    def runGame(self):
+        self.getList()
+        for a in range(10):
+
+            self.getGameValues()
+
+            self.getQuestionVals()
+
+            self.getInput()
+
+            self.checkIfRight()
+
+        print("You got", self.points, "/ 10 right.")
+        if self.points == 10:
+            print("Good Job! You are great at this.")
+        elif self.points > 7:
+            print("You did good but it could be better.")
+        elif self.points > 3:
+            print("You suck, and should study more.")
+        else:
+            print("Call your local doctor to check you for a mental disability.")
 
 
-listdata = getList()
-
-
-def getGameValues(listdata):
-    samplelist = []
-    for idx in range(3):
-        if idx == 0:
-            i = random.randrange((len(listdata)))
-            k = random.randrange(0, 5)
-            j = "Present"
-            doc, form = i, k
-        if idx == 1:
-            j = "Preterite"
-        if idx == 2:
-            j = "Imperfect"
-        samplelist.append(listdata[i][j][k])
-    if samplelist[0] == samplelist[1]:
-        samplelist = samplelist.pop(0)
-
-    for idx in range(8-len(samplelist)):
-        k = random.randrange(0, 5)
-        j = random.choice(["Present", "Preterite", "Imperfect"])
-        while listdata[i][j][k] in samplelist:
-            k = random.randrange(0, 5)
-            j = random.choice(["Present", "Preterite", "Imperfect"])
-        samplelist.append(listdata[i][j][k])
-
-    # checks repeating
-
-    right = samplelist[random.randrange(3)]
-    if right == samplelist[0]:
-        tense = "Present"
-    if right == samplelist[1]:
-        tense = "Preterite"
-    if right == samplelist[2]:
-        tense = "Imperfect"
-
-    random.shuffle(samplelist)
-
-    return doc, tense, form, samplelist, right
-
-
-def getQuestionVals(j, k):
-    if j == "Present":
-        tense = "present"
-    if j == "Preterite":
-        tense = "preterite"
-    if j == "Imperfect":
-        tense = "imperfect"
-    if k == 0:
-        form = "yo"
-    if k == 1:
-        form = "tu"
-    if k == 2:
-        form = random.choice(["el", "ella", "usted"])
-    if k == 3:
-        form = "nosotros"
-    if k == 4:
-        form = random.choice(["ellos", "ellas", "ustedes"])
-    return tense, form
-
-
-def getInput(tense, right, samplelist, listdata, idx, form):
-    guess = input("What is the "+tense+" tense of "+listdata[idx]["Infinitive"]+" in the "+form+"? \nA: "+samplelist[0]+"\nB: "+samplelist[1] + "\nC: " +
-                  samplelist[2] + "\nD: " + samplelist[3] + "\nE: " + samplelist[4]+"\nF: " + samplelist[5]+"\nG: " + samplelist[6]+"\nH: " + samplelist[7]+"\n")
-    if right == samplelist[0]:
-        let = "A"
-    if right == samplelist[1]:
-        let = "B"
-    if right == samplelist[2]:
-        let = "C"
-    if right == samplelist[3]:
-        let = "D"
-    if right == samplelist[4]:
-        let = "E"
-    if right == samplelist[5]:
-        let = "F"
-    if right == samplelist[6]:
-        let = "G"
-    if right == samplelist[7]:
-        let = "H"
-    return guess, let
-
-
-def checkIfRight(guess, let):
-    global points
-    rightlist = ["Good Job! You got it right!", "Nice! That's right!", "That is the correct answer!",
-                 "You are good at this!", "Wow! That was cool how you got it right!"]
-    wronglist = ["You suck! That was the wrong answer!", "How did you miss that! That was easy!",
-                 "That is the wrong answer.", "That is completely incorrect!", "Are you stupid? That was wrong!"]
-    if guess == let or guess == let.lower():
-        points += 1
-        betweenStatements(random.choice(rightlist))
-    else:
-        betweenStatements(random.choice(wronglist))
-
-
-for a in range(10):
-
-    idx, j, k, samplelist, right = getGameValues(listdata)
-
-    tense, form = getQuestionVals(j, k)
-
-    guess, let = getInput(tense, right, samplelist, listdata, idx, form)
-
-    checkIfRight(guess, let)
-
-
-print("You got", points, "/ 10 right.")
-if points == 10:
-    print("Good Job! You are great at this.")
-elif points > 7:
-    print("You did good but it could be better.")
-elif points > 3:
-    print("You suck, and should study more.")
-else:
-    print("Call your local doctor to check you for a mental disability.")
+game = Game()
+game.runGame()
--- spanishvocab.py
+++ spanishvocab.py
@@ -3,118 +3,119 @@ import os
 import random
 import verborganizer
 import sys
-vocablist = []
-vlist = []
-nlist = []
-points = 0
+
 
 # functions
 
 
-def betweenStatements(statement):
-    print("\n")
-    print(statement)
-    input("\nPress Enter to Continue:")
+class Game:
 
+    def __init__(self):
+        self.listdata = []
+        self.level = 0
+        self.vlist = []
+        self.nlist = []
+        self.level = input(
+            "What is the difficulty of words you want? 1 for easiest, 3 for hardest: ")
+        self.points = 0
 
-def getList():
-    if len(sys.argv) == 1:
-        listdata = verborganizer.build("SpanishWords")
-    elif len(sys.argv) == 2:
-        try:
-            listdata = verborganizer.build(sys.argv[1])
-        except FileNotFoundError:
-            print("That is an invalid directory.")
-            quit()
-    return listdata
-
-
-def getDifficulty(level):
-    if level == "1":
-        num = 1
-    if level == "2":
-        num = 2
-    if level == "3":
-        num = 3
-    return num
-
-
-def convertList(verbs, nouns, listdata, level):
-    for verb in listdata["Verbs"]:
-        if getDifficulty(level) == verb["Difficulty"]:
-            verbs.append(verb["Infinitive"])
-    for noun in listdata["Nouns"]:
-        if getDifficulty(level) == noun["Difficulty"]:
-            nouns.append(noun["Spanish"])
-
-
-def getGameValues(vlist, listdata):
-    lchoice = random.choice([vlist, nlist])
-    samplelist = random.sample(lchoice, 5)
-
-    word = random.choice(samplelist)
-    if lchoice == vlist:
-        for verb in listdata["Verbs"]:
-            if word == verb["Infinitive"]:
-                english = verb["English"]
-    if lchoice == nlist:
-        for noun in listdata["Nouns"]:
-            if word == noun["Spanish"]:
-                english = noun["English"]
-    return word, english, samplelist
-
-
-def getUserGuess(eng, samplelist, word):
-    guess = input("What is the spanish word for "+eng+"? \nA: " +
-                  samplelist[0]+"\nB: "+samplelist[1] + "\nC: " + samplelist[2] + "\nD: " + samplelist[3] + "\nE: " + samplelist[4]+"\n\n")
-    if word == samplelist[0]:
-        let = "A"
-    if word == samplelist[1]:
-        let = "B"
-    if word == samplelist[2]:
-        let = "C"
-    if word == samplelist[3]:
-        let = "D"
-    if word == samplelist[4]:
-        let = "E"
-    return let, guess
-
-
-def checkIfRight(guess, let):
-    rightlist = ["Good Job! You got it right!", "Nice! That's right!", "That is the correct answer!",
-                 "You are good at this!", "Wow! That was cool how you got it right!"]
-    wronglist = ["That was the wrong answer! Nice try though.", "How did you miss that! That was easy!",
-                 "That is the wrong answer.", "That is completely incorrect!", "That was wrong!"]
-    global points
-    if guess == let or guess == let.lower():
-        points += 1
+    def betweenStatements(self, statement):
         print("\n")
-        betweenStatements(random.choice(rightlist))
-    else:
-        betweenStatements(random.choice(wronglist))
+        print(statement)
+        input("\nPress Enter to Continue:")
+
+    def getList(self):
+        if len(sys.argv) == 1:
+            self.listdata = verborganizer.build("SpanishWords")
+        elif len(sys.argv) == 2:
+            try:
+                self.listdata = verborganizer.build(sys.argv[1])
+            except FileNotFoundError:
+                print("That is an invalid directory.")
+                quit()
+        return self.listdata
+
+    def getDifficulty(self, level):
+        if self.level == "1":
+            self.num = 1
+        if self.level == "2":
+            self.num = 2
+        if self.level == "3":
+            self.num = 3
+        return self.num
+
+    def convertList(self):
+        for verb in self.listdata["Verbs"]:
+            if self.getDifficulty(self.level) == verb["Difficulty"]:
+                self.vlist.append(verb["Infinitive"])
+        for noun in self.listdata["Nouns"]:
+            if self.getDifficulty(self.level) == noun["Difficulty"]:
+                self.nlist.append(noun["Spanish"])
+        print(self.vlist)
+
+    def getGameValues(self):
+        self.lchoice = random.choice([self.vlist, self.nlist])
+        self.samplelist = random.sample(self.lchoice, 5)
+
+        self.word = random.choice(self.samplelist)
+        if self.lchoice == self.vlist:
+            for verb in self.listdata["Verbs"]:
+                if self.word == verb["Infinitive"]:
+                    self.english = verb["English"]
+        if self.lchoice == self.nlist:
+            for noun in self.listdata["Nouns"]:
+                if self.word == noun["Spanish"]:
+                    self.english = noun["English"]
+
+    def getUserGuess(self):
+        self.guess = input("What is the spanish word for " + self.english+"? \nA: " +
+                           self.samplelist[0]+"\nB: " + self.samplelist[1] + "\nC: " + self.samplelist[2] + "\nD: " + self.samplelist[3] + "\nE: " + self.samplelist[4]+"\n\n")
+        if self.word == self.samplelist[0]:
+            self.let = "A"
+        if self.word == self.samplelist[1]:
+            self.let = "B"
+        if self.word == self.samplelist[2]:
+            self.let = "C"
+        if self.word == self.samplelist[3]:
+            self.let = "D"
+        if self.word == self.samplelist[4]:
+            self.let = "E"
+
+    def checkIfRight(self):
+        self.rightlist = ["Good Job! You got it right!", "Nice! That's right!", "That is the correct answer!",
+                          "You are good at this!", "Wow! That was cool how you got it right!"]
+        self.wronglist = ["That was the wrong answer! Nice try though.", "How did you miss that! That was easy!",
+                          "That is the wrong answer.", "That is completely incorrect!", "That was wrong!"]
+        if self.guess == self.let or self.guess == self.let.lower():
+            self.points += 1
+            print("\n")
+            self.betweenStatements(random.choice(self.rightlist))
+        else:
+            self.betweenStatements(random.choice(self.wronglist))
+
+    def runGame(self):
+
+        self.listdata = self.getList()
+        self.convertList()
+        for e in range(10):
+
+            self.getGameValues()
+
+            self.getUserGuess()
+
+            self.checkIfRight()
+
+        print("You got", self.points, "/ 10 right.")
+
+        if self.points == 10:
+            print("Good Job! You are great at this.")
+        elif self.points > 7:
+            print("You did good but it could be better.")
+        elif self.points > 3:
+            print("You suck, and should study more.")
+        else:
+            print("Call your local doctor to check you for a mental disability.")
 
 
-listdata = getList()
-level = input(
-    "What is the difficulty of words you want? 1 for easiest, 3 for hardest: ")
-convertList(vlist, nlist, listdata, level)
-print(vlist)
-
-for e in range(10):
-
-    word, eng, samplelist = getGameValues(vlist, listdata)
-
-    let, guess = getUserGuess(eng, samplelist, word)
-
-    checkIfRight(guess, let)
-
-print("You got", points, "/ 10 right.")
-
-if points == 10:
-    print("Good Job! You are great at this.")
-elif points > 7:
-    print("You did good but it could be better.")
-elif points > 3:
-    print("You suck, and should study more.")
-else:
-    print("Call your local doctor to check you for a mental disability.")
+game = Game()
+game.runGame()
