dced5b7160701b16a3fa52394039a9504fad596a
ikale1234 <55333848+ikale1234@users.noreply.github.com>
2020-04-25 17:20:39 -0700

    made inheriting classes

--- conjugation.py
+++ conjugation.py
@@ -5,11 +5,62 @@ import sys
 points = 0
 
 
-class Game:
+class consoleView:
     def __init__(self):
+        self.rightlist = ["Good Job! You got it right!", "Nice! That's right!",
+                          "That is the correct answer!", "You are good at this!", "Wow! That was cool how you got it right!"]
+        self.wronglist = ["You suck! That was the wrong answer!", "How did you miss that! That was easy!",
+                          "That is the wrong answer.", "That is completely incorrect!", "Are you stupid? That was wrong!"]
+
+    def betweenStatements(self, statement):
+        print("\n")
+        print(statement)
+        input("\nPress Enter to Continue:")
+
+    def getInput(self, tense, listdata, samplelist, right, i, form):
+        self.guess = input("What is the "+tense+" tense of "+listdata[i]["Infinitive"]+" in the "+form+"? \nA: "+samplelist[0]+"\nB: "+samplelist[1] + "\nC: " +
+                           samplelist[2] + "\nD: " + samplelist[3] + "\nE: " + samplelist[4]+"\nF: " + samplelist[5]+"\nG: " + samplelist[6]+"\nH: " + samplelist[7]+"\n")
+        if right == samplelist[0]:
+            self.let = "A"
+        if right == samplelist[1]:
+            self.let = "B"
+        if right == samplelist[2]:
+            self.let = "C"
+        if right == samplelist[3]:
+            self.let = "D"
+        if right == samplelist[4]:
+            self.let = "E"
+        if right == samplelist[5]:
+            self.let = "F"
+        if right == samplelist[6]:
+            self.let = "G"
+        if right == samplelist[7]:
+            self.let = "H"
+
+    def displayResult(self, correct):
+        if correct:
+            self.betweenStatements(random.choice(self.rightlist))
+        else:
+            self.betweenStatements(random.choice(self.wronglist))
+
+    def whenGameOver(self, points):
+        print("You got", points, "/ 10 right.")
+        if points == 10:
+            print("Good Job! You are great at this.")
+        elif points > 7:
+            print("You did good but it could be better.")
+        elif points > 3:
+            print("You suck, and should study more.")
+        else:
+            print("Call your local doctor to check you for a mental disability.")
+
+
+class Game:
+    def __init__(self, view):
         self.points = 0
         self.listdata = []
         self.points = 0
+        self.view = view
 
     def betweenStatements(self, statement):
         print("\n")
@@ -84,36 +135,13 @@ class Game:
         if self.rform == 4:
             self.form = random.choice(["ellos", "ellas", "ustedes"])
 
-    def getInput(self):
-        self.guess = input("What is the "+self.tense+" tense of "+self.listdata[self.i]["Infinitive"]+" in the "+self.form+"? \nA: "+self.samplelist[0]+"\nB: "+self.samplelist[1] + "\nC: " +
-                           self.samplelist[2] + "\nD: " + self.samplelist[3] + "\nE: " + self.samplelist[4]+"\nF: " + self.samplelist[5]+"\nG: " + self.samplelist[6]+"\nH: " + self.samplelist[7]+"\n")
-        if self.right == self.samplelist[0]:
-            self.let = "A"
-        if self.right == self.samplelist[1]:
-            self.let = "B"
-        if self.right == self.samplelist[2]:
-            self.let = "C"
-        if self.right == self.samplelist[3]:
-            self.let = "D"
-        if self.right == self.samplelist[4]:
-            self.let = "E"
-        if self.right == self.samplelist[5]:
-            self.let = "F"
-        if self.right == self.samplelist[6]:
-            self.let = "G"
-        if self.right == self.samplelist[7]:
-            self.let = "H"
-
     def checkIfRight(self):
-        self.rightlist = ["Good Job! You got it right!", "Nice! That's right!", "That is the correct answer!",
-                          "You are good at this!", "Wow! That was cool how you got it right!"]
-        self.wronglist = ["You suck! That was the wrong answer!", "How did you miss that! That was easy!",
-                          "That is the wrong answer.", "That is completely incorrect!", "Are you stupid? That was wrong!"]
-        if self.guess == self.let or self.guess == self.let.lower():
+
+        if self.view.guess == self.view.let or self.view.guess == self.view.let.lower():
             self.points += 1
-            self.betweenStatements(random.choice(self.rightlist))
+            self.correct = True
         else:
-            self.betweenStatements(random.choice(self.wronglist))
+            self.correct = False
 
     def runGame(self):
         self.getList()
@@ -123,20 +151,16 @@ class Game:
 
             self.getQuestionVals()
 
-            self.getInput()
+            self.view.getInput(self.tense, self.listdata,
+                               self.samplelist, self.right, self.i, self.form)
 
             self.checkIfRight()
 
-        print("You got", self.points, "/ 10 right.")
-        if self.points == 10:
-            print("Good Job! You are great at this.")
-        elif self.points > 7:
-            print("You did good but it could be better.")
-        elif self.points > 3:
-            print("You suck, and should study more.")
-        else:
-            print("Call your local doctor to check you for a mental disability.")
+            self.view.displayResult(self.correct)
+
+        self.view.whenGameOver(self.points)
 
 
-game = Game()
+view = consoleView()
+game = Game(view)
 game.runGame()
--- spanishvocab.py
+++ spanishvocab.py
@@ -8,16 +8,63 @@ import sys
 # functions
 
 
+class consoleView:
+    def __init__(self):
+        self.rightlist = ["Good Job! You got it right!", "Nice! That's right!",
+                          "That is the correct answer!", "You are good at this!", "Wow! That was cool how you got it right!"]
+        self.wronglist = ["You suck! That was the wrong answer!", "How did you miss that! That was easy!",
+                          "That is the wrong answer.", "That is completely incorrect!", "Are you stupid? That was wrong!"]
+
+    def askDifficulty(self):
+        self.level = input(
+            "What is the difficulty of words you want? 1 for easiest, 3 for hardest: ")
+
+    def betweenStatements(self, statement):
+        print("\n")
+        print(statement)
+        input("\nPress Enter to Continue:")
+
+    def getInput(self, english, samplelist, word):
+        self.guess = input("What is the spanish word for " + english+"? \nA: " +
+                           samplelist[0]+"\nB: " + samplelist[1] + "\nC: " + samplelist[2] + "\nD: " + samplelist[3] + "\nE: " + samplelist[4]+"\n\n")
+        if word == samplelist[0]:
+            self.let = "A"
+        if word == samplelist[1]:
+            self.let = "B"
+        if word == samplelist[2]:
+            self.let = "C"
+        if word == samplelist[3]:
+            self.let = "D"
+        if word == samplelist[4]:
+            self.let = "E"
+
+    def displayResult(self, correct):
+        if correct:
+            self.betweenStatements(random.choice(self.rightlist))
+        else:
+            self.betweenStatements(random.choice(self.wronglist))
+
+    def whenGameOver(self, points):
+        print("You got", points, "/ 10 right.")
+        if points == 10:
+            print("Good Job! You are great at this.")
+        elif points > 7:
+            print("You did good but it could be better.")
+        elif points > 3:
+            print("You suck, and should study more.")
+        else:
+            print("Call your local doctor to check you for a mental disability.")
+
+
 class Game:
 
-    def __init__(self):
+    def __init__(self, view):
         self.listdata = []
         self.level = 0
         self.vlist = []
         self.nlist = []
-        self.level = input(
-            "What is the difficulty of words you want? 1 for easiest, 3 for hardest: ")
         self.points = 0
+        self.view = view
 
     def betweenStatements(self, statement):
         print("\n")
@@ -36,20 +83,20 @@ class Game:
         return self.listdata
 
     def getDifficulty(self, level):
-        if self.level == "1":
+        if level == "1":
             self.num = 1
-        if self.level == "2":
+        if level == "2":
             self.num = 2
-        if self.level == "3":
+        if level == "3":
             self.num = 3
         return self.num
 
     def convertList(self):
         for verb in self.listdata["Verbs"]:
-            if self.getDifficulty(self.level) == verb["Difficulty"]:
+            if self.getDifficulty(self.view.level) == verb["Difficulty"]:
                 self.vlist.append(verb["Infinitive"])
         for noun in self.listdata["Nouns"]:
-            if self.getDifficulty(self.level) == noun["Difficulty"]:
+            if self.getDifficulty(self.view.level) == noun["Difficulty"]:
                 self.nlist.append(noun["Spanish"])
         print(self.vlist)
 
@@ -67,55 +114,32 @@ class Game:
                 if self.word == noun["Spanish"]:
                     self.english = noun["English"]
 
-    def getUserGuess(self):
-        self.guess = input("What is the spanish word for " + self.english+"? \nA: " +
-                           self.samplelist[0]+"\nB: " + self.samplelist[1] + "\nC: " + self.samplelist[2] + "\nD: " + self.samplelist[3] + "\nE: " + self.samplelist[4]+"\n\n")
-        if self.word == self.samplelist[0]:
-            self.let = "A"
-        if self.word == self.samplelist[1]:
-            self.let = "B"
-        if self.word == self.samplelist[2]:
-            self.let = "C"
-        if self.word == self.samplelist[3]:
-            self.let = "D"
-        if self.word == self.samplelist[4]:
-            self.let = "E"
-
     def checkIfRight(self):
-        self.rightlist = ["Good Job! You got it right!", "Nice! That's right!", "That is the correct answer!",
-                          "You are good at this!", "Wow! That was cool how you got it right!"]
-        self.wronglist = ["That was the wrong answer! Nice try though.", "How did you miss that! That was easy!",
-                          "That is the wrong answer.", "That is completely incorrect!", "That was wrong!"]
-        if self.guess == self.let or self.guess == self.let.lower():
+        if self.view.guess == self.view.let or self.view.guess == self.view.let.lower():
             self.points += 1
             print("\n")
-            self.betweenStatements(random.choice(self.rightlist))
+            self.correct = True
         else:
-            self.betweenStatements(random.choice(self.wronglist))
+            self.correct = False
 
     def runGame(self):
 
+        self.view.askDifficulty()
         self.listdata = self.getList()
         self.convertList()
         for e in range(10):
 
             self.getGameValues()
 
-            self.getUserGuess()
+            self.view.getInput(self.english, self.samplelist, self.word)
 
             self.checkIfRight()
 
-        print("You got", self.points, "/ 10 right.")
+            self.view.displayResult(self.correct)
 
-        if self.points == 10:
-            print("Good Job! You are great at this.")
-        elif self.points > 7:
-            print("You did good but it could be better.")
-        elif self.points > 3:
-            print("You suck, and should study more.")
-        else:
-            print("Call your local doctor to check you for a mental disability.")
+        self.view.whenGameOver(self.points)
 
 
-game = Game()
+view = consoleView()
+game = Game(view)
 game.runGame()
